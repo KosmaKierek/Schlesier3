8ba48350d7a55d82ba0d8681be6c77102d87d531
student <student@student.uj.edu.pl>
2024-04-24 09:41:14 +0200

    aa

---
+++ app/src/Controller/RecordController.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Record controller.
+ */
+
+namespace App\Controller;
+
+use App\Repository\RecordRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * Class RecordController.
+ */
+#[Route('/record')]
+class RecordController extends AbstractController
+{
+    /**
+     * Index action.
+     *
+     * @param RecordRepository $repository Record repository
+     *
+     * @return Response HTTP response
+     */
+    #[Route(
+        name: 'record_index',
+        methods: 'GET'
+    )]
+    public function index(RecordRepository $repository): Response
+    {
+        $records = $repository->findAll();
+
+        return $this->render(
+            'record/index.html.twig',
+            ['records' => $records]
+        );
+    }
+
+    #[Route(
+        '/{id}',
+        name: 'record_show',
+        requirements: ['id' => '[1-9]\d*'],
+        methods: 'GET'
+    )]
+    public function show(int $id): Response
+    {
+
+        $records = $repository->findOneByID($id);
+
+        return $this->render(
+            'record/show.html.twig',
+            ['record' => $record]
+        );
+        return new Response(content: 'Record details #'.$id);
+    }
+}
+
